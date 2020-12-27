3a7ea78e6db382971bf0a8e3dd9668219cc6fe4c
Mohamed Gamil <gemy.dev@gmail.com>
2020-12-27 14:12:42 +0200

    upgrade version to 0.0.16 + AppBear API class refactoring

--- includes/class-appbear-api.php
+++ includes/class-appbear-api.php
@@ -11,15 +11,17 @@ class AppbearAPI {
   const VERYIFY_SSL = false;
 
   /**
-   * Class constructor
+   * Options & Headers internal stores
+   *
+   * @var array
    */
-	public function __construct( $args = array() ) {
-    // ...
-  }
+  protected static $_opts = array();
+  protected static $_headers = array();
 
   /**
    * Check a given license key for validity
    *
+   * @TODO: Needs further refactoring
    * @param string $licenseKey
    * @return array|WP_ERROR The response or WP_Error on failure.
    */
@@ -31,12 +33,17 @@ class AppbearAPI {
       'url'       => home_url()
     );
 
-    return wp_remote_post( APPBEAR_STORE_URL, array( 'timeout' => static::TIMEOUT_DURATION, 'sslverify' => static::VERYIFY_SSL, 'body' => $api_params ) );
+    return wp_remote_post( APPBEAR_STORE_URL, array(
+      'timeout' => static::TIMEOUT_DURATION,
+      'sslverify' => static::VERYIFY_SSL,
+      'body' => $api_params,
+    ));
   }
 
   /**
    * Activate a given license key
    *
+   * @TODO: Needs further refactoring
    * @param string $licenseKey
    * @return array|WP_ERROR The response or WP_Error on failure.
    */
@@ -67,29 +74,69 @@ class AppbearAPI {
    * @return array|WP_ERROR The response or WP_Error on failure.
    */
   public static function send_notification($title, $body, $type = 'post', $ID = '') {
+    $endpoint = '/wp-json/appbear-edd-addon/v1/notifications';
     $params = array(
+      'title' => $title,
+      'body' => $body,
       'notification' => array(
         'title' => $title,
         'body' => $body,
       ),
       'data' => array(
         'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
+        'title' => $title,
         'type' => $type,
         'id' => $ID,
       ),
     );
 
-    $endpoint = APPBEAR_STORE_URL . '/wp-json/appbear-edd-addon/v1/notifications';
-    $opts = array(
-      'timeout' => static::TIMEOUT_DURATION,
-      'sslverify' => static::VERYIFY_SSL,
-      'body' => json_encode($params),
-      'headers' => array(
-        'Content-Type' => 'application/json; charset=utf-8',
-        'X-AppBear-Key' => appbear_get_public_key(),
-      ),
+    return static::_send( $endpoint, $params );
+  }
+
+  /**
+   * Apply request common headers & options
+   *
+   * @param boolean $includeAuthHeaders  Include authentication headers (TRUE / FALSE)
+   * @param boolean $forceDisableSSLVerification  Force disable SSL Verification (TRUE / FALSE)
+   * @param boolean $timeoutDuration  Override default timeout duration
+   */
+  protected static function _headers($includeAuthHeaders = true, $forceDisableSSLVerification = null, $timeoutDuration = null) {
+    $SSLVerification = is_null($forceDisableSSLVerification) ? static::VERYIFY_SSL : $forceDisableSSLVerification === true;
+    $timeout = is_null($timeoutDuration) ? static::TIMEOUT_DURATION : absint($timeoutDuration);
+    $headers = array(
+      'Content-Type' => 'application/json; charset=utf-8',
     );
 
-    return wp_remote_post( $endpoint, $opts );
+    if ($includeAuthHeaders) {
+      $headers['X-AppBear-Key'] = appbear_get_public_key();
+    }
+
+    static::$_opts = array(
+      'timeout' => $timeout,
+      'sslverify' => $SSLVerification,
+    );
+
+    static::$_headers = $headers;
+  }
+
+  /**
+   * Send request using `wp_remote_post`,
+   *
+   * @param string $endpoint Endpoint relative URL
+   * @param array $body Request parameters
+   * @param boolean $includeAuthHeaders Include authentication headers (TRUE / FALSE)
+   * @return array|WP_ERROR The response or WP_Error on failure.
+   */
+  protected static function _send($endpoint = '', $body = array(), $includeAuthHeaders = true) {
+    $headers = static::_headers($includeAuthHeaders);
+    $opts = array_merge(
+      static::$_opts,
+      array(
+        'headers' => $headers,
+        'body' => json_encode($body),
+      )
+    );
+
+    return wp_remote_post( APPBEAR_STORE_URL . $endpoint, $opts );
   }
 }
