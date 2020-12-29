328659ffc64e5a388660d0a92c38a399b650a3dc
Mohamed Gamil <gemy.dev@gmail.com>
2020-12-29 10:16:26 +0200

    debug: options

--- includes/class-admin-page.php
+++ includes/class-admin-page.php
@@ -1396,8 +1396,8 @@ class AdminPage extends AppbearCore {
           $options['validConfig'] = true;
 
           // NOTE: Debug lines
+          dd($options, $responseObject);
           // dd($public_key, $this->_getLicenseKey(), $responseObject);
-          // dd($options);
           // echo json_encode($options);die;
 
           update_option( 'appbear-options', $options );
