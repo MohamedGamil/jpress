b127ae5fd8623044e7da66a2e584d0b677ff5dfe
Mohamed Gamil <gemy.dev@gmail.com>
2020-12-30 14:42:41 +0200

    debug: remove appbear connect

--- constants.php
+++ constants.php
@@ -93,7 +93,7 @@ define( 'APPBEAR_ENABLE_DEBUG_HELPERS', true );
 /**
  * Enable / Disable Displaying Connect to AppBear even if the license is active
  */
-define( 'APPBEAR_ENABLE_CONNECT_PAGE_IF_ACTIVE', true );
+define( 'APPBEAR_ENABLE_CONNECT_PAGE_IF_ACTIVE', false );
 
 
 /**
